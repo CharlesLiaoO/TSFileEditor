568487e7d04be68a75919a365c8925f871b80991
LiaoCheng <LiaoChengO@qq.com>
2023-06-01 16:01:23 +0800

    转义开头的"和'

--- MainApps/TSFileEditor/DataModel/TranslateModel.h
+++ MainApps/TSFileEditor/DataModel/TranslateModel.h
@@ -17,13 +17,28 @@ public:
     void SetTranslate(const QString &str, bool es=false);
 
     static QString ToEscapeStr(QString str) {
-        str.replace("\n", "\\n");
-        str.replace("\t", "\\t");
+//        str.prepend("'");  //excel单元格编辑之后，才会消失显示'，不便于复制粘贴
+        if (str.startsWith("\'"))  //只替换开头的
+            str.replace(0, 1, R"(\')");
+        if (str.startsWith("\""))
+            str.replace(0, 1, R"(\")");
+//        str.replace("\'", R"(\')");
+//        str.replace("\"", R"(\")");
+        str.replace("\n", R"(\n)");
+        str.replace("\t", R"(\t)");
         return str;
     }
     static QString FromEscapeStr(QString str) {
-        str.replace("\\n", "\n");
-        str.replace("\\t", "\t");
+        if (str.startsWith("'"))
+            str.remove(0, 1);  //防止翻译人员自己加'
+        if (str.startsWith(R"(\')"))  //只替换开头的
+            str.replace(0, 1, "\'");
+        if (str.startsWith(R"(\")"))
+            str.replace(0, 1, "\"");
+//        str.replace(R"(\')", "\'");
+//        str.replace(R"(\")", "\"");
+        str.replace(R"(\n)", "\n");
+        str.replace(R"(\t)", "\t");
         return str;
     }
 private:
